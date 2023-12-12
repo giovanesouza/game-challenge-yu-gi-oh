d273f1c95ac87bdd9eb2075e91f05fe1b50b9c08
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-12 15:11:35 -0300

    feat: enumeração de cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,7 +18,36 @@ const state = {
     actions: {
         button: document.getElementById('next-duel'),
     }
-}
+};
+
+const pathImages = "./src/assets/icons/";
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper", // Utilizado para definir quem vai ganhar
+        img: `${pathImages}dragon.png`,
+        WinOf: [1], // Ganha para a carta de id 1
+        LoseOf: [2] // perde para a carta com id 2
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0]
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1]
+    },
+];
 
 
 // Chama o estado inicial do jogo
